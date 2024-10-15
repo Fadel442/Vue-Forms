a6e818baf7be27ea2b5c62e26996600344933e26
Fadel442 <fadelmuhammad4402@gmail.com>
2024-10-15 12:41:51 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import SignupForm from './components/komponen/SignupForm.vue'
+</script>
+
+<template>
+  <SignupForm />
+</template>
+
+<script>
+  export default {
+    components: { SignupForm },
+    data() {
+      return {
+        
+      }
+    },
+  }
+</script>
+
+<style>
+body {
+  margin: 0;
+  background: gray;
+}
+</style>
